2ff9a51de086a28dbe8e68910e60596562baeb03
Soniacarrero <81909697+Soniacarrero@users.noreply.github.com>
2023-05-07 18:56:39 +0200

    Update Juego.cpp

--- Juego.cpp
+++ Juego.cpp
@@ -1,129 +1,97 @@
-#include "freeglut.h"
+#include <GL/glut.h>
 #include "Coordenada.h"
 #include "Tablero.h"
-#include <iostream>
 #include <string>
+#include <iostream>
+
 using namespace std;
 
-void Menu();
-void OnDraw(void);
-
 const int TAM_CASILLA = 75;
+void menu(int value) {
+    switch (value) {
+    case 1:
+        std::cout << "Has seleccionado: Nueva partida" << std::endl;
+        // Aquí va el código para iniciar una nueva partida
+        break;
+    case 2:
+        std::cout << "Has seleccionado: Cargar partida" << std::endl;
+        // Aquí va el código para cargar una partida guardada
+        break;
+    case 3:
+        std::cout << "Has seleccionado: Guardar partida" << std::endl;
+        // Aquí va el código para guardar la partida actual
+        break;
+    case 4:
+        std::cout << "Has seleccionado: Salir del juego" << std::endl;
+        // Aquí va el código para salir del juego
+        exit(0);
+        break;
+    }
+}
+void display() {
 
-Tablero tablero;
+    glClear(GL_COLOR_BUFFER_BIT);
+    // Dibujar el borde del tablero
+    glColor3f(0.4f, 0.2f, 0.0f);
+    glRecti(-75, -75, TAM_CASILLA * 10, TAM_CASILLA * 10);
 
-int main(int argc,char* argv[]) {
-	glutInit(&argc, argv);
-	glutInitWindowSize(800, 800);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutCreateWindow("Jaque al descubierto");
-	glClearColor(0, 0, 1, 1);
-	glColor3f(1.f, 0, 0);
-	glOrtho(800, 0, 800, 0, -1, 1);
-	
-	//Registrar los callbacks
-	glutDisplayFunc(OnDraw);
-	
-	Menu();
-	
-	tablero.inicio();
-	tablero.setCoord();
-	
-	glutMainLoop();
+    // Dibujar posiciones del tablero
+    glColor3f(1.0f, 1.0f, 1.0f);
 
-    	return 0;
+    for (int i = 0; i < TAM_TABLERO; i++) {
+
+        char letra = 'a';
+
+        for (int j = 0; j < 2; j = j++) {
+
+            glRasterPos2i((i + 1) * TAM_CASILLA + TAM_CASILLA / 2, j * TAM_CASILLA * (TAM_TABLERO + 1) + TAM_CASILLA / 2);
+            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, letra + i);
+            glRasterPos2i(TAM_CASILLA / 2 + j * TAM_CASILLA * (TAM_TABLERO + 1), (i + 1) * TAM_CASILLA + TAM_CASILLA / 2);
+            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, '8' - i);
+        }
+    }
+
+
+    // Dibujar el tablero
+    for (int fila = 1; fila <= TAM_TABLERO; fila++) {
+
+        for (int columna = 1; columna <= TAM_TABLERO; columna++) {
+
+            if ((fila + columna) % 2 == 0) {
+                glColor3f(0.8f, 0.6f, 0.4f);
+            }
+            else {
+                glColor3f(0.6f, 0.4f, 0.2f);
+            }
+
+            glRecti(columna * TAM_CASILLA, fila * TAM_CASILLA,
+                (columna + 1) * TAM_CASILLA, (fila + 1) * TAM_CASILLA);
+        }
+    }
+
+    glFlush();
 }
 
-void OnDraw(void)
+int main(int argc, char* argv[])
 {
-	//Borrado de la pantalla	
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(TAM_CASILLA * TAM_TABLERO, TAM_CASILLA * TAM_TABLERO);
+    glutCreateWindow("Ajedrez");
 
-	//Para definir el punto de vista
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	
-	Menu();
-	tablero.dibuja();
-	//pieza.dibuja();
-
-	//no borrar esta linea ni poner nada despues
-	glutSwapBuffers();
-	
-}
-
-void Menu()
-{
-
-    glClear(GL_COLOR_BUFFER_BIT); // Limpia la pantalla
-    
-    // Dibuja el título del juego
     glMatrixMode(GL_PROJECTION);
-    glPushMatrix();
     glLoadIdentity();
-    gluOrtho2D(0, 640, 0, 480);
-    glMatrixMode(GL_MODELVIEW);
-    glPushMatrix();
-    glLoadIdentity();
-    glColor3f(1, 1, 1);
-    glRasterPos2f(250, 400);
-    string titulo = "Jaque al descubierto";
-    for (int i = 0; i < titulo.length(); i++) {
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, titulo[i]);
-    }
-    glPopMatrix();
-    glMatrixMode(GL_PROJECTION);
-    glPopMatrix();
-    
-    // Dibuja las opciones del menú
-    glMatrixMode(GL_PROJECTION);
-    glPushMatrix();
-    glLoadIdentity();
-    gluOrtho2D(0, 640, 0, 480);
-    glMatrixMode(GL_MODELVIEW);
-    glPushMatrix();
-    glLoadIdentity();
-    glColor3f(1, 1, 1);
-    glRasterPos2f(250, 300);
-    string opcion1 = "1. Nueva partida";
-    for (int i = 0; i < opcion1.length(); i++) {
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, opcion1[i]);
-    }
-    glRasterPos2f(250, 250);
-    string opcion2 = "2. Salir";
-    for (int i = 0; i < opcion2.length(); i++) {
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, opcion2[i]);
-    }
-    glPopMatrix();
-    glMatrixMode(GL_PROJECTION);
-    glPopMatrix();
-    
-    // Dibuja todo en la pantalla
-    glutSwapBuffers();
-}
+    glOrtho(0, TAM_CASILLA * TAM_TABLERO + 150, TAM_CASILLA * TAM_TABLERO + 150, 0, -1, 1);
+    // Crear el menú
+    glutCreateMenu(menu);
+    glutAddMenuEntry("Nueva partida", 1);
+    glutAddMenuEntry("Cargar partida", 2);
+    glutAddMenuEntry("Guardar partida", 3);
+    glutAddMenuEntry("Salir", 4);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
+    glutDisplayFunc(display);
 
-// Función que maneja el evento de selección de una opción del menú
-void menu(int opcion) 
-{
-    switch(opcion) 
-    {
-        case 1:
-            // Pedimos los nombres de los jugadores
-            /*string nombreJugador1, nombreJugador2;
-            cout << "Ingrese el nombre del jugador 1: ";
-            cin >> nombreJugador1;
-            cout << "Ingrese el nombre del jugador 2: ";
-            cin >> nombreJugador2;*/
-            
-            // Iniciamos el juego
-            // Aquí iría el código para el juego 
-            break;
-        case 2:
-            // Salimos del programa
-            exit(0);
-            break;
-        default:
-            // Si el usuario ingresa una opción inválida, no hacemos nada
-            break;
-    }
+    glutMainLoop();
+
+    return 0;
 }
