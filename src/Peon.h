f7ac3385396df6b7c5e93abde1f63133eafed6ca
Eugenio-Fernandez <80359021+Eugenio-Fernandez@users.noreply.github.com>
2023-05-13 18:44:00 +0200

    Update Peon.h

--- src/Peon.h
+++ src/Peon.h
@@ -1,6 +1,18 @@
 #pragma once
-class Peon
-{
-  virtual ~Peon();
+#include "Pieza.h"
+#include "Tablero.h"
+
+class Peon : public Pieza {
+public:
+    Peon(Color color, Coordenada posicion) : Pieza(color, posicion) {};
+    ~Peon() {};
+    
+    // Implementación de la validación de movimiento para el peón
+    bool esMovimientoValido(Coordenada origen, Coordenada destino, Tablero& tablero) const ;
+    // Implementación de la obtención de todas las jugadas posibles para el peón
+    vector<Coordenada> obtenerTodasLasJugadasPosibles(Tablero& tablero) const ;
+    void mover(unsigned char key);
 };
+
+
 
