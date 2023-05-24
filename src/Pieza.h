b4a6b9c0ac69605a73b5659c9e623ab208d9d9fb
Franxcus <80366737+Franxcus@users.noreply.github.com>
2023-05-24 18:38:22 +0200

    Update Pieza.h

--- src/Pieza.h
+++ src/Pieza.h
@@ -1,12 +1,11 @@
 #pragma once
 #include <vector>
-
 #include "Coordenada.h"
-#include "Tablero.h"
+#include "ETSIDI.h"
+#include "Interaccion.h"
+using ETSIDI::SpriteSequence;
 
 using namespace std;
-
-class Tablero;
 
 enum class Color {
     Blanco,
@@ -14,24 +13,27 @@ enum class Color {
 };
 
 class Pieza {
-public:
-
-    //"virtual" --> lo hace modificable al método por otras clases, se implementa en las clases derivadas
-    //"const = 0" --> hace obligatorio que las clases derivadas la implementen
-
-    Pieza(Color color, Coordenada posicion);
-    virtual ~Pieza();
-
-    Color obtenerColor() const;
-    Coordenada obtenerPosicionActual() const;
-    //Introduzco nueva coordenada y se actualiza
-    void actualizarPosicion(Coordenada posicion);
-    virtual bool esMovimientoValido(Coordenada origen, Coordenada destino, Tablero& tablero) const = 0;
-    virtual vector<Coordenada> obtenerTodasLasJugadasPosibles(Tablero& tablero) const = 0;
-    
-
-private:
-    Color color;
+    friend class Interaccion;
     Coordenada posicion;
+protected:
+    int color;// 0 blanca 1 negra
+    Coordenada posicion;
+    SpriteSequence sprite1{ "imagenes/AlfilBlanco.png",1 };
+    SpriteSequence sprite2{ "imagenes/CaballoBlanco.png",1 };
+    SpriteSequence sprite3{ "imagenes/PeonBlanco.png",1 };
+    SpriteSequence sprite4{ "imagenes/ReinaBlanca.png",1 };
+    SpriteSequence sprite5{ "imagenes/ReyBlanco.png",1 };
+    SpriteSequence sprite6{ "imagenes/TorreBlanca.png",1 };
+    SpriteSequence sprite7{ "imagenes/AlfilNegro.png",1 };
+    SpriteSequence sprite8{ "imagenes/CaballoNegro.png",1 };
+    SpriteSequence sprite9{ "imagenes/PeonNegro.png",1 };
+    SpriteSequence sprite10{ "imagenes/ReinaNegra.png",1 };
+    SpriteSequence sprite11{ "imagenes/ReyNegro.png",1 };
+    SpriteSequence sprite12{ "imagenes/TorreNegra.png",1 };
+public:
+    Pieza();
+    //virtual ~Pieza();
+    virtual void dibuja();
+    virtual void mueve(Coordenada origen, Coordenada destino);
+    Coordenada getPos();
 };
-
