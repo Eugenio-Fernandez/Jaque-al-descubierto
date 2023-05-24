8ba9a3fa607e08a28caa6e9e9940b3b1ebf0d7ec
Franxcus <80366737+Franxcus@users.noreply.github.com>
2023-05-24 18:37:23 +0200

    Update Pieza.cpp

--- src/Pieza.cpp
+++ src/Pieza.cpp
@@ -1,39 +1,19 @@
-#pragma once
-#include <vector>
-#include "Coordenada.h"
+#include "Pieza.h"
+#include "Tablero.h"
 #include "ETSIDI.h"
-#include "Interaccion.h"
+#include "freeglut.h"
 using ETSIDI::SpriteSequence;
 
-using namespace std;
 
-enum class Color {
-    Blanco,
-    Negro
-};
+Pieza::Pieza(){
 
-class Pieza {
-    friend class Interaccion;
-    Coordenada posicion;
-protected:
-    int color;// 0 blanca 1 negra
-    Coordenada posicion;
-    SpriteSequence sprite1{ "imagenes/AlfilBlanco.png",1 };
-    SpriteSequence sprite2{ "imagenes/CaballoBlanco.png",1 };
-    SpriteSequence sprite3{ "imagenes/PeonBlanco.png",1 };
-    SpriteSequence sprite4{ "imagenes/ReinaBlanca.png",1 };
-    SpriteSequence sprite5{ "imagenes/ReyBlanco.png",1 };
-    SpriteSequence sprite6{ "imagenes/TorreBlanca.png",1 };
-    SpriteSequence sprite7{ "imagenes/AlfilNegro.png",1 };
-    SpriteSequence sprite8{ "imagenes/CaballoNegro.png",1 };
-    SpriteSequence sprite9{ "imagenes/PeonNegro.png",1 };
-    SpriteSequence sprite10{ "imagenes/ReinaNegra.png",1 };
-    SpriteSequence sprite11{ "imagenes/ReyNegro.png",1 };
-    SpriteSequence sprite12{ "imagenes/TorreNegra.png",1 };
-public:
-    Pieza();
-    //virtual ~Pieza();
-    virtual void dibuja();
-    virtual void mueve(Coordenada origen, Coordenada destino);
-    Coordenada getPos();
-};
+}
+
+void Pieza::dibuja() {
+	
+}
+
+
+Coordenada Pieza::getPos() {
+	return posicion;
+}
