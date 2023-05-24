124c8faf667794d1b9ff9197e8fbee802584577d
Franxcus <80366737+Franxcus@users.noreply.github.com>
2023-05-24 18:39:24 +0200

    Add files via upload

--- src/ListaPiezas.cpp
+++ src/ListaPiezas.cpp
@@ -1,49 +1,35 @@
 #include "ListaPiezas.h"
-ListaPiezas::~ ListaPiezas()
-{
-}
+
 ListaPiezas::ListaPiezas()
 {
-	numero = 0;
-	for (int i = 0; i < 32; i++)
+	n = 0;
+	for (int i = 0; i < MAX_PIEZAS; i++)
 		lista[i] = 0;
 }
 
+//Funcion para agregar una nueva pieza a la lista (develve TRUE si hay espacio en la lista)
+bool ListaPiezas::agregar(Pieza* t)
+{
+	if (n < MAX_PIEZAS) {
+		lista[n++] = t;// último puesto sin rellenar
+	}
+	else
+		return false; // capacidad máxima alcanzada
+	return true;
+}
+
+//Funcion que nos permite dibujar las distintas piezas
 void ListaPiezas::dibuja()
 {
-	for (int i = 0; i < numero; i++) {
-
+	for (int i = 0; i < n; i++) {
 		lista[i]->dibuja();
 	}
 
 }
+/*
+void ListaPiezas::inicializa() {
+	Coordenada pos = {0,0};
+	Torre* aux = new Torre(pos);
+	lista[0]->agregar(aux);
+}*/
 
-
-void ListaPiezas::eliminar(int index)
-{
-	if ((index < 0) || (index >= numero))
-		return;
-	delete lista[index];
-	numero--;
-	for (int i = index; i < numero; i++)
-		lista[i] = lista[i + 1];
-}
-
-//Funcion que evalua si debe eliminar una pieza y llama a la funcion eliminar que las elimina
-void ListaPiezas::eliminar(Pieza* e)
-{
-	for (int i = 0; i < numero; i++)
-		if (lista[i] == e)
-		{
-			eliminar(i);
-			ETSIDI::play("sonidos/impacto.wav");
-			return;
-		}
-}
-
-//Funcion para eliminar el contenido de las piezas
-void ListaPiezas::eliminarContenido() {
-	for (int i = 0; i < 32; i++) //eliminacion de las piezas
-		lista[i] = 0;
-	numero = 0;
-}
