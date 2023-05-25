27b0af4a33c5d42b8c4fd82740a415bc54115b3c
Soniacarrero <81909697+Soniacarrero@users.noreply.github.com>
2023-05-25 13:29:11 +0200

    Update Menu.cpp

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -1,8 +1,13 @@
 #include "Menu.h"
 #include "freeglut.h"
 #include "ETSIDI.h"
+#include "Menu.h"
+#include "freeglut.h"
+#include "ETSIDI.h"
+#include <iostream>
+#include "Tablero.h"
 using ETSIDI::Sprite;
-
+Tablero  tablero2;
 Menu::Menu()
 {
 	menu1->setSize(8.0, 8.0);
@@ -43,4 +48,46 @@ void Menu::flecha(int key) {
 			break;
 		}
 	}
+}
+
+void op(int value) {
+
+	switch (value) {
+	case 1:
+		std::cout << "Has seleccionado: Nueva partida" << std::endl;
+		// Aquí va el código para iniciar una nueva partida
+		//void dibuja();
+		tablero2.dibuja_selector();
+		tablero2.piezas.dibuja();
+		tablero2.dibuja();
+
+		//tablero2.dibuja();
+		//tablero2.dibuja_selector();
+		break;
+	case 2:
+		std::cout << "Has seleccionado: Cargar partida" << std::endl;
+		// Aquí va el código para cargar una partida guardada
+		break;
+	case 3:
+		std::cout << "Has seleccionado: Guardar partida" << std::endl;
+		// Aquí va el código para guardar la partida actual
+		break;
+	case 4:
+		std::cout << "Has seleccionado: Salir del juego" << std::endl;
+		// Aquí va el código para salir del juego
+		exit(0);
+		break;
+	case 5:
+		std::cout << "Has seleccionado: Menu" << std::endl;
+		// Aquí va el código para volver a sacar el menu
+		//Menu();
+		//~Menu();
+		void dibuja();
+		void inicializa();
+		void tecla(unsigned char key);
+		void tecla2(unsigned char key);
+		break;
+	}
+
+
 }
