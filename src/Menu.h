9772c6a359a26dc5e9faca0e829e8a4dc0b4abcc
Soniacarrero <81909697+Soniacarrero@users.noreply.github.com>
2023-05-25 13:48:37 +0200

    Update Menu.h

--- src/Menu.h
+++ src/Menu.h
@@ -5,8 +5,8 @@ using ETSIDI::Sprite;
 class Menu
 {
 private:
-	Sprite* menu1 = new Sprite("imagenes/AlfilBlanco.png", 4.0, 4.0, 8.0, 8.0);
-	Sprite* menu2 = new Sprite("imagenes/CaballoBlanco.png", 4.0, 4.0, 8.0, 8.0);
+	Sprite* menu1 = new Sprite("imagenes/menunuevapartida.png", 4.0, 4.0, 8.0, 8.0);
+	Sprite* menu2 = new Sprite("imagenes/menusalir.png", 4.0, 4.0, 8.0, 8.0);
 	bool start = false;
 	int pantalla = 0;
 public:
