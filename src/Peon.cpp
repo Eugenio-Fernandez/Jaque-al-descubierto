ce50089688ed662224c3110bfd7b91e066d7881f
Franxcus <80366737+Franxcus@users.noreply.github.com>
2023-05-25 17:20:03 +0200

    Add files via upload

--- src/Peon.cpp
+++ src/Peon.cpp
@@ -9,7 +9,6 @@ Peon::Peon(Coordenada posicion_, int color_, int casilla_)
 	posicion.y = posicion_.y;
 	color = color_;
 	casilla = casilla_;
-	tipo = 4;
 }
 
 Peon::~Peon()
@@ -35,11 +34,18 @@ void Peon::dibuja() {
 
 void Peon::movimientovalido(int origen, int destino, bool& b) {
 	int dif = destino - origen;
+	std::cout << origen << endl;
 	std::cout << dif << endl;
-	if (((dif%7==0)||(dif%8==0)||(dif%9==0))&&((dif<10)&&(dif>-10))&&(((dif>0)&&(color == 0))||((dif < 0) && (color == 1)))) {
+
+	if (
+		(dif % 8 == 0) && ((dif < 10) && (dif > -10)) && (((dif > 0) && (color == 0)) || ((dif < 0) && (color == 1)))
+		||
+		((dif % 16 == 0) && ((origen >= 8) && (origen <= 15)) || ((origen >= 48) && (origen <= 55)))
+		) {
 		b = TRUE;
 	}
 	else {
 		b = FALSE;
+
 	}
 }
