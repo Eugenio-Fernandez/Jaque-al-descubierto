621957701952ff26e1a69f245191cb8731ed4549
Eugenio-Fernandez <80359021+Eugenio-Fernandez@users.noreply.github.com>
2023-05-26 22:56:21 +0200

    Update Torre.cpp

--- src/Torre.cpp
+++ src/Torre.cpp
@@ -3,8 +3,18 @@
 
 Torre::Torre(Coordenada posicion_, int color_, int casilla_)
 {
-	sprite6.setSize(0.25, 0.25);
-	sprite12.setSize(0.25, 0.25);
+	conf_modo();
+
+	if (modo == 1)
+		sprite6.setSize(0.25, 0.25);
+	if (modo == 2)
+		sprite6_1.setSize(1.0, 1.0);
+
+	if (modo == 1)
+		sprite12.setSize(0.25, 0.25);
+	if (modo == 2)
+		sprite12_1.setSize(1.0, 1.0);
+
 	posicion.x = posicion_.x;
 	posicion.y = posicion_.y;
 	color = color_;
@@ -18,16 +28,38 @@ Torre::~Torre()
 }
 
 void Torre::dibuja() {
+
+
 	if (color == 0) {
-		sprite6.setCenter((float)posicion.x, (float)posicion.y);
+
+		if (modo == 1)
+			sprite6.setCenter((float)posicion.x, (float)posicion.y);
+		if (modo == 2)
+			sprite6_1.setCenter((float)posicion.x + 0.3775, (float)posicion.y + 0.3775);
+
 		glPushMatrix();
-		sprite6.draw();
+
+		if (modo == 1)
+			sprite6.draw();
+		if (modo == 2)
+			sprite6_1.draw();
+
 		glPopMatrix();
 	}
 	if (color == 1) {
-		sprite12.setCenter((float)posicion.x, (float)posicion.y);
+
+		if (modo == 1)
+			sprite12.setCenter((float)posicion.x, (float)posicion.y);
+		if (modo == 2)
+			sprite12_1.setCenter((float)posicion.x + 0.3775, (float)posicion.y + 0.3775);
+
 		glPushMatrix();
-		sprite12.draw();
+
+		if (modo == 1)
+			sprite12.draw();
+		if (modo == 2)
+			sprite12_1.draw();
+
 		glPopMatrix();
 	}
 }
