ad2a532a1e3f8ecb04e99e3045296d36d6660f0d
Soniacarrero <81909697+Soniacarrero@users.noreply.github.com>
2023-05-26 18:26:27 +0200

    Update Rey.cpp

--- src/Rey.cpp
+++ src/Rey.cpp
@@ -35,6 +35,7 @@ void Rey::movimientovalido(int origen, int destino, bool& b) {
 	int dif = destino - origen;
 	if (((dif % 7 == 0) || (dif % 8 == 0) || (dif % 9 == 0)||(dif==1)||(dif==-1))&&(dif<10)&&(dif>-10)) {
 		b=TRUE;
+		ETSIDI::play("sonidos/mov.wav");
 	}
 	else {
 		b=FALSE;
