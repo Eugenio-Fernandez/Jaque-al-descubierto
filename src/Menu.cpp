2618e0cd3ff753cc86205983e336a1df996a3a24
Eugenio-Fernandez <80359021+Eugenio-Fernandez@users.noreply.github.com>
2023-05-26 22:50:34 +0200

    Update Menu.cpp

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -3,6 +3,7 @@
 #include "ETSIDI.h"
 #include "cstdlib"
 #include "Tablero.h"
+#include <iostream>
 using ETSIDI::Sprite;
 Tablero tablero2;
 
@@ -12,55 +13,86 @@ Menu::Menu()
 	menu1->setPos(4.0, 4.0);
 	menu2->setSize(2.0, 2.0);
 	menu2->setPos(4.0, 4.0);
+	menu3->setSize(2.0, 2.0);
+	menu3->setPos(4.0, 4.0);
+	menu4->setSize(2.0, 2.0);
+	menu4->setPos(4.0, 4.0);
 }
 
 Menu::~Menu() {
 	menu1->~Sprite();
 	menu2->~Sprite();
+	menu3->~Sprite();
+	menu4->~Sprite();
 }
-
-void Menu::flecha(int key) {
-
-	if (start == false) {
-		switch (key) {
-		case GLUT_KEY_UP:
-			if (pantalla == 0) {
-				pantalla = 1;
-			}
-			break;
-
-		case GLUT_KEY_DOWN:
-			if (pantalla == 1) {
-				pantalla = 0;
-			}
-			break;
-		}
-	}
-}
-
-void Menu::dibuja()
+int Menu::dibuja()
 {
+	int a = 0; // Inicializamos la variable a con un valor por defecto
 
 	if (inicio == 0) {
+		if (modo3 == 0) {
+			if (modo1 == 1) {
+				//menu1->draw();
+			}
+			else if (modo1 == 2) {
+				//menu2->draw();
+			}
 
-		if (modo1 == 1)menu1->draw();
-		if (modo1 == 2)menu2->draw();
-		if (modo1 == 1 && modo2 == 1) {
-
-			ETSIDI::play("sonidos/cr7.wav");
-			modo1 = 0;
-			inicio = 1;
-			delete menu1;
-			delete menu2;
+			if (modo2 == 1 && modo1 == 2) {
+				inicio = 1;
+			}
+			else if (modo2 == 1 && modo1 == 1) {
+				modo2 = 0;
+				modo3 = 1;
+			}
 		}
 
-		if (modo1 == 2 && modo2 == 1 && inicio == 0) {
-			exit(0);
+		if (modo3 == 1) {
+
+			if (modo1 == 1) {
+				//menu3->draw();
+
+			}
+			else if (modo1 == 2) {
+				//menu4->draw();
+				a = 2;
+			}
+			if (modo2 == 1) {
+				std::cout << "esto" << std::endl;
+				ETSIDI::play("sonidos/cr7.wav");
+				marcha = 1;
+				modo3 = 3;
+				delete menu1;
+				delete menu2;
+				delete menu3;
+				delete menu4;
+			}
 		}
 	}
+	else if (inicio == 1) {
+		exit(0);
+	}
+
+	if (modo2 == 1) {
+		if (modo1 == 1) {
+			marcha = 1;
+			return 1;
+		}
+		else if (modo1 == 2) {
+			return 2;
+		}
+	}
+
+	// Forzar manualmente el valor de retorno para probar modos, no ha sido correctamente implementado
+	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	////////////////////////////////////////
+
+	return 2; // 1 --> Modo Clásico; 2 --> Modo ETSIDI
+
+	////////////////////////////////////////
+	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 }
-
-
 
 //Funcion para inicializar el selector
 void Menu::inicializa() {
