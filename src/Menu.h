0bc18a663ba88742e19a233726a4d7eb8a9db142
Eugenio-Fernandez <80359021+Eugenio-Fernandez@users.noreply.github.com>
2023-05-26 22:49:45 +0200

    Update Menu.h

--- src/Menu.h
+++ src/Menu.h
@@ -5,20 +5,31 @@ using ETSIDI::Sprite;
 class Menu
 {
 private:
+	
 
 	bool start = false;
 	Sprite* menu1 = new Sprite("imagenes/menunuevapartida.png", 4.0, 4.0, 10.0, 10.0);
 	Sprite* menu2 = new Sprite("imagenes/menusalir.png", 4.0, 4.0, 10.0, 10.0);
-	//bool inicio;
-	int modo1 = 1, modo2, inicio = 0;
+	Sprite* menu3 = new Sprite("imagenes/modoclasico.png", 4.0, 4.0, 10.0, 10.0);
+	Sprite* menu4 = new Sprite("imagenes/modoetsidi.png", 4.0, 4.0, 10.0, 10.0);
+
+
+	int modo1 = 1, modo2, modo3 = 0, inicio = 0, a = 1;
 	int pantalla = 0;
+
 public:
+
+	int marcha;
+
 	Menu();
 	~Menu();
-	void dibuja();
+	int dibuja();
 	void flecha(int key);
 	void inicializa();
 	void tecla(unsigned char key);
 	void tecla2(unsigned char key);
-	void op(int value);
+
+	int getMarcha() { return dibuja(); }
+
 };
+
