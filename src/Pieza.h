1dda780d141ffdb199f4a57524d7c44f8700afb3
Eugenio-Fernandez <80359021+Eugenio-Fernandez@users.noreply.github.com>
2023-05-26 23:18:33 +0200

    Update Pieza.h

--- src/Pieza.h
+++ src/Pieza.h
@@ -2,6 +2,8 @@
 #include <vector>
 #include "Coordenada.h"
 #include "ETSIDI.h"
+#include "Menu.h"
+#include <iostream>
 
 using ETSIDI::SpriteSequence;
 
@@ -25,6 +27,8 @@ Torre = 6
 class Pieza {
     
 protected:
+    Menu menu;
+    int modo;
     int casilla;
     int color;// 0 blanca 1 negra
     int tipo;
@@ -43,6 +47,19 @@ protected:
     SpriteSequence sprite10{ "imagenes/ReinaNegra.png",1 };
     SpriteSequence sprite11{ "imagenes/ReyNegro.png",1 };
     SpriteSequence sprite12{ "imagenes/TorreNegra.png",1 };
+
+    SpriteSequence sprite1_1{ "imagenes/alfilblancoetsidi.png",1 };
+    SpriteSequence sprite2_1{ "imagenes/caballoblancoetsidi.png",1 };
+    SpriteSequence sprite3_1{ "imagenes/peonblancoetsidi.png",1 };
+    SpriteSequence sprite4_1{ "imagenes/reinablancaetsidi.png",1 };
+    SpriteSequence sprite5_1{ "imagenes/reyblancoetsidi.png",1 };
+    SpriteSequence sprite6_1{ "imagenes/torreblancaetsidi.png",1 };
+    SpriteSequence sprite7_1{ "imagenes/alfilnegroetsidi.png",1 };
+    SpriteSequence sprite8_1{ "imagenes/caballonegroetsidi.png",1 };
+    SpriteSequence sprite9_1{ "imagenes/peonnegroetsidi.png",1 };
+    SpriteSequence sprite10_1{ "imagenes/reinanegraetsidi.png",1 };
+    SpriteSequence sprite11_1{ "imagenes/reynegroetsidi.png",1 };
+    SpriteSequence sprite12_1{ "imagenes/torrenegraetsidi.png",1 };
 public:
     Pieza();
     //virtual ~Pieza();
@@ -59,4 +76,5 @@ public:
     void setPieza_movida() { pieza_movida = TRUE; }
     void setPieza_nomovida() { pieza_movida = FALSE; }
     void setPieza_enemiga_pieza(bool pieza_enemiga_) { pieza_enemiga_pieza = pieza_enemiga_; }
+    void conf_modo() { modo = menu.getMarcha(); }
 };
